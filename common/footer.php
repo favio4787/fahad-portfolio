566bc95be3614efe09ac810c4864680916f33cf0
favio4787 <fahad@softwarealliance.com>
2025-01-31 20:09:25 +0500

    fixes

---
+++ common/footer.php
@@ -0,0 +1,30 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="col1">
+                    <h2>Let’s talk about your project</h2>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                    <form class="form row">
+                        <div class="form-group col-md-4">
+                            <input type="text" class="form-control" placeholder="First Name">
+                        </div>
+                        <div class="form-group col-md-8">
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-12">
+                            <button type="submit" class="btn">Send Message</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="img-box">
+                    <img src="images/map.jpg">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="images/favicon.png">
+    <title>Fahad-Frontend Developer</title>
+    <link media="all" rel="stylesheet" type="text/css" href="css/main.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <header id="header" class="canvas__wrapper">
+            <canvas id="canvas"></canvas>
+            <div class="nav-sidebar">
+                <a href="#" class="nav-opener"><i class="icon-bars"></i> </a>
+                <nav id="nav">
+                    <a href="#" class="nav-opener"><i class="fal fa-close"></i></a>
+                    <div class="nav-box">
+                        <h1>Fahad</h1>
+                        <ul class="nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">About me</a></li>
+                            <li><a href="#">Video</a> </li>
+                            <li><a href="#">Hire me</a> </li>
+                        </ul>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/fahadnadeem"><i class="fab fa-linkedin-in"></i> </a></li>
+                            <li><a href="skype:live:.cid.123456789?chat"><i class="fab fa-skype"></i> </a></li>
+                            <li><a href="mailto:nfahad4787@gmail.com">nfahad4787@gmail.com</a> </li>
+                            <li><a href="#"> 0335 8735635</a> </li>
+                        </ul>
+                    </div>
+                </nav>
+                <ul class="social-links">
+                    <li><a href="https://www.linkedin.com/in/fahadnadeem"><i class="fab fa-linkedin-in"></i> </a></li>
+                    <li><a href="skype:live:.cid.123456789?chat"><i class="fab fa-skype"></i> </a></li>
+                </ul>
+            </div>
+            <div class="top-header">
+                <div class="container">
+                    <div class="line-box crawlline">
+                        <div class="line-wrap">
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+                            <p>Let's talk about your new project</p>
+
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-square">
+                        <i class="icon-email"></i>
+                        Hire me
+                    </a>
+                </div>
+            </div>
+            <div class="main-header">
+                <div class="container">
+                    <div class="logo">
+                        <a href="#">
+                            <svg viewBox="0 0 100 100" width="110" height="110">
+                                <defs>
+                                    <path id="circle"
+                                        d="
+                                M 50, 50
+                                m -37, 0
+                                a 37,37 0 1,1 74,0
+                                a 37,37 0 1,1 -74,0" />
+                                </defs>
+                                <text font-size="16">
+                                    <textPath xlink:href="#circle">
+                                        Frontend - Developer
+                                    </textPath>
+                                </text>
+                            </svg>
+                            <i class="icon-mapmarker"></i>
+                        </a>
+                    </div>
+                    <div class="banner-text">
+                        <h1>Hi, <br>
+                            I’m <strong>Fahad </strong> Nadeem <br>
+                            <div class="typewrite" data-period="2000" data-type='[ " Build interactive UIs. ", " create seamless experiences. ", " design responsive layouts. ", " optimize website performance. " ]'>
+                                <span class="wrap"></span>
+                            </div>
+                        </h1>
+                        My Clients:
+                        <div class="logo-slider">
+                            <div>
+                                <div class="logo-box">
+                                    <img src="images/logic-wind.svg">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="logo-box">
+                                    <img src="images/neo.svg">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="logo-box">
+                                    <img src="images/meme.png">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="logo-box">
+                                    <img src="images/lumis.svg">
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="award-holder">
+                <div class="container">
+                    <a href="#" class="award-btn"><i class="fal fa-times"></i> </a>
+                    <div class="row">
+                        <div class="col col-md-6">
+                            <h1>Achievements & Awards</h1>
+                            <p>Successfully delivered 50+ high-quality web development projects, leading to a 95% client satisfaction rate. Recognized for optimizing website performance, improving user engagement, and delivering responsive designs using modern frontend technologies.</p>
+
+                            <ul class="social-links">
+                                <li><a href="https://www.linkedin.com/in/fahadnadeem"><i class="fab fa-linkedin-in"></i> </a></li>
+                                <li><a href="skype:live:.cid.123456789?chat"><i class="fab fa-skype"></i> </a></li>
+                                <li><a href="mailto:nfahad4787@gmail.com">nfahad4787@gmail.com</a> </li>
+                                <li><a href="tel:+923358735635"> 03358735635</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col col-md-6">
+                            <ul class="award-list">
+                                <li><span>Excellence</span></li>
+                                <li><span>Freelancer</span></li>
+                                <li><span>Satisfaction</span></li>
+                                <li><span>Optimization</span></li>
+                                <li><span>Achievement</span></li>
+                                <li><span>Delivery</span></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="award-btn">
+                <span>Awards <img src="images/award.svg"></span>
+            </a>
+
+        </header>
+
+        <main id="main">
+            <section class="section">
+                <div class="container">
+                    <div class="row about align-items-center mb-5">
+                        <div class="col col-md-6">
+                            <h1>About me.</h1>
+                            <p>A Passionate Front-End Developer with over 3+ years of experience in designing, developing, and deploying cutting-edge web applications. With a keen eye for detail and user experience, I have helped several organizations create dynamic, responsive, and scalable websites and applications using modern technologies like React.js, Vue.js, and Next.js.</p>
+                            <p>Highly skilled in front-end development, UI/UX design, and responsive web design. Throughout my career, I've worked closely with cross-functional teams to ensure seamless user experiences and efficient codebase management, consistently delivering high-quality products.</p>
+                            <p>A trusted collaborator and strategic thinker, always focused on translating business needs into functional, efficient, and visually appealing web solutions. My expertise lies in bridging the gap between design and development to create exceptional user interfaces.</p>
+                            <p>
+                                <span class="text-blue">Let’s talk about your next web project.</span><br>
+                                Bring me your ideas, and I’ll bring them to life with clean, scalable, and efficient code.
+                            </p>
+
+                            <a href="tel:03358735635" class="btn">Let's Talk</a>
+                        </div>
+                        <div class="col col-md-6">
+                            <div id='tech-js' style="font-size: 22px; font-weight: bold;"></div>
+                        </div>
+                    </div>
+                    <div class="row text-center mb-5">
+                        <div class="col-md-3 mb-3">
+                            <h2 class="text-blue">3+</h2>
+                            <h5>Year of experience</h5>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <h2 class="text-blue">100+</h2>
+                            <h5>Complete Projects</h5>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <h2 class="text-blue">MANY</h2>
+                            <h5>Happy Clients</h5>
+                        </div>
+
+                        <div class="col-md-3">
+                            <h2 class="text-blue">10+</h2>
+                            <h5>Awards</h5>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <section class="section portfolio">
+                <div class="container">
+                    <div class="section-header">
+                        <h1>My Portfolio.</h1>
+                        <p>Here is the portfolio of my most recent successful projects.</p>
+                        <p>Started from scratch and made them a winning product in the online market with my design, marketing, and management expertise. </p>
+                    </div>
+                </div>
+                <div class="profile-holder">
+                    <div class="row">
+                        <div class="col col-md-6">
+                            <div id="list-example" class="list-group">
+                                <a class="list-group-item list-group-item-action active" href="#list-item-1"><img src="images/sa-logo.svg"></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-2"><img src="images/meme.png"></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-3"><img src="images/lumis.svg"></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-4"><img src="images/neo.svg"></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-5"><img src="images/logic-wind.svg"></a>
+                            </div>
+                        </div>
+                        <div class="col col-md-6">
+                            <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+                                <!-- Software Alliance Project -->
+                                <div id="list-item-1" class="box active">
+                                    <h5 class="text-blue">WEB & MOBILE APPLICATIONS</h5>
+                                    <h2>Software Alliance - IT Company Portfolio</h2>
+                                    <p>Developed and maintained the company’s portfolio website using Next.js and Firebase, improving SEO by 30% and boosting site traffic. Ensured optimal performance, SEO, and a seamless user experience with a focus on responsive design, scalability, and accessibility. Leveraged Redux for state management and SCSS for efficient styling and layout.</p>
+                                </div>
+                                <!-- Memee Project -->
+                                <div id="list-item-2" class="box active">
+                                    <h5 class="text-blue">WEB APPLICATION</h5>
+                                    <h2>Memee - Meme Website</h2>
+                                    <p>Developed a meme website using React.js with real-time updates and user-generated content. Utilized Redux for state management and SCSS for styling, ensuring a responsive and efficient platform. Focused on delivering a seamless user experience with optimized performance.</p>
+                                </div>
+                                <!-- Lumis Pro Consulting SP ZOO -->
+                                <div id="list-item-3" class="box active">
+                                    <h5 class="text-blue">WEB DESIGN</h5>
+                                    <h2>Lumis Pro Consulting SP ZOO - Corporate Website Template</h2>
+                                    <p>Designed and developed a professional corporate website template for Lumis Pro Consulting. Incorporated animations using JavaScript for a dynamic user experience. Utilized SCSS for modular and efficient styling, ensuring responsiveness and optimal performance across devices.</p>
+                                </div>
+                                <!-- Neoman Project -->
+                                <div id="list-item-4" class="box active">
+                                    <h5 class="text-blue">WEB APPLICATION</h5>
+                                    <h2>Neoman - News Blog Website</h2>
+                                    <p>Developed a news blog website using Next.js, integrating Redux for efficient state management across components. Implemented dynamic content loading and real-time updates to ensure fresh and engaging user experiences. Focused on responsive design and optimized performance, ensuring seamless browsing across devices. Applied best practices for SEO to improve visibility and user engagement.</p>
+                                </div>
+                                <!-- Logic Wind Solutions Project -->
+                                <div id="list-item-5" class="box active">
+                                    <h5 class="text-blue">WEB DESIGN</h5>
+                                    <h2>Logic Wind Solutions - Business Solutions Template</h2>
+                                    <p>Designed and developed a professional template for Logic Wind Solutions, highlighting their expertise in business growth and transformative technologies. Focused on strategic implementation and long-term value, helping businesses enhance operational efficiency and gain a competitive advantage. Created a modern, responsive design with optimized performance for seamless user experience across devices.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            <section class="section c-info">
+                <div class="container">
+                    <div class="section-header">
+                        <div class="c-logo">
+                            <a href="https://www.linkedin.com/in/fahad-nadeem-b811321a9/">
+                                <img style="border-radius: 50%;" src="images/fahad.jpg" alt="Your Logo" />
+                            </a>
+                        </div>
+                        <h1>Fahad Nadeem</h1>
+                        <p>Frontend Developer | React.js, Next.js, Redux Expert</p>
+                        <p>With over 3 years of experience in frontend development, I specialize in creating dynamic, responsive websites using modern technologies such as React.js, Next.js, and Redux. My focus is on optimizing website performance, implementing SEO strategies, and ensuring seamless user experiences across devices.</p>
+                        <p>I'm passionate about bringing innovative solutions to life and collaborating with cross-functional teams to deliver high-quality, scalable projects. Let's work together to bring your ideas to life!</p>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+        <script type="text/javascript" src='js/tech-js.js'></script>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/slick.js"></script>
+    <script type="text/javascript" src="js/mouse-trail.min.js"></script>
+
+
+</body>
+
+</html>
